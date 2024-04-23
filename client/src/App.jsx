f6382c6b675bf6e30c3ce5f03684a511126a5f2c
shahnoorgit <shahnoormujawar@gmail.com>
2024-04-23 19:49:36 +0530

    Home page started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./component/Home";
+import Home from "./pages/Home";
+import Navbar from "./component/Navbar";
+import Report from "./pages/Report";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/my-report" element={<Report />} />
       </Routes>
     </>
   );
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosLogOut, IoMdLogOut } from "react-icons/io";
+import { VscSignIn } from "react-icons/vsc";
 
 const Navbar = () => {
-  return <div>Navbarook</div>;
+  const [isUser, setisUser] = useState(false);
+  return (
+    <nav className="flex justify-between p-5 items-center w-full h-20 bg-gray-700 border-blue-700 shadow-2xl border">
+      <a href="/" className="font-bold text-white text-2xl">
+        <span className="text-blue-500">Track</span>
+        <span>HR</span>
+      </a>
+      <a
+        href="/my-report"
+        className="font-bold text-xl text-white border p-2 hover:bg-white hover:text-gray-900 border-blue-600"
+      >
+        My Report
+      </a>
+      <div className="flex gap-2 items-center cursor-pointer">
+        {!isUser && (
+          <a href="/login">
+            <div className=" flex justify-center items-center gap-2 cursor-pointer">
+              <p className=" text-white font-semibold text-xl">Login</p>
+              <VscSignIn className="text-white text-xl font-bold" />
+            </div>
+          </a>
+        )}
+        {isUser && (
+          <>
+            <img
+              src="./demoUser.png"
+              className=" object-contain rounded-full"
+              width={40}
+              height={40}
+            />
+            <p className=" text-xl flex gap-1 items-center text-white max-sm:text-lg">
+              logout
+              <IoMdLogOut />
+            </p>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
