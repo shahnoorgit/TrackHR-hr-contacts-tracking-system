20687471e4022c1ab3b591774c08d4ff0417ec5b
shahnoorgit <shahnoormujawar@gmail.com>
2024-04-23 16:08:25 +0530

    Client added

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./component/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
